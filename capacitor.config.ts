90de345c9abf9996f68765b6c9a131de0f9e58d1
MDecker-MobileComputing <mide_42b@yahoo.de>
2022-05-04 12:47:18 +0200

    Initialer Stand nach Erzeugung Projekt

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.mide.ionic.plugindemo',
+  appName: 'Plugin-Demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
